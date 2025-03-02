4389219ad2501ef91b3b21b6f5d9fe6e27d4ed9f
feb3rk <efeberksk@gmail.com>
2025-03-02 17:39:43 +0300

    Add files via upload

---
+++ ocakodev2.py
@@ -0,0 +1,8 @@
+sayi = int(input("lütfen bir sayı giriniz: "))
+if sayi < 0:
+     print("negatif sayı girdiniz!")
+else:
+        if sayi % 2 == 0:
+            print("seçtiğiniz sayı çift sayıdır.")
+        else:
+            print("seçtiğiniz sayı tek sayıdır.")
